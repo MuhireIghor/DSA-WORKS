86aca1e421f8ab524e39d0d0c424b9e198f5414b
MuhireIghor <muhireighor123@gmail.com>
2022-11-11 09:36:20 +0200

    sort optimisation

--- soringAlgos/bubbleSort.cpp
+++ soringAlgos/bubbleSort.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main(){
 	int i,n=4,temp,j;
 	int num[4]={12,55,5,18};
+	int check=0;
 	for(i=0;i<n-1;i++){
 		cout<<"i ="<<i<<endl;
 		for(j=0;j<n-1;j++){
@@ -10,6 +11,7 @@ int main(){
 				temp=num[j];
 				num[j]=num[j+1];
 				num[j+1]=temp;
+				check=1;
 			}
 			cout<<endl;
 			cout<<"j="<<j<<endl;
@@ -17,7 +19,12 @@ int main(){
 				cout<<num[p]<<"\t";
 
 			}
+			cout<<endl;
 		}		
+			//Before the next  iteration if the value of check is changed to 1 , we break;
+			if(check==0){
+				break;
+			}
 	}
 cout<<"[";
 for(int k=0;k<n;k++){
