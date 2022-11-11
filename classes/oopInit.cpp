12c2baf47bf6206de61780c5ff474475f0a376d6
MuhireIghor <muhireighor123@gmail.com>
2022-11-11 15:58:55 +0200

    member function declaration out of the class

--- classes/oopInit.cpp
+++ classes/oopInit.cpp
@@ -3,14 +3,21 @@ using namespace std;
 class Student {
     public:
     string name;
+    int age;
     void printName(){
         cout<<"The student name is "<<name;
     }
+    void sayAge();
 };
+void Student::sayAge(){
+cout<<"The student is "<<age<<" years old";
+}
 int main(){
 Student s1;
 s1.name = "Muhire Ighor";
+s1.age=17;
 s1.printName();
+s1.sayAge();
     return 0;
 
 }
