00ebb3f7154a7393b8a7260ccaa30c1fee9f6f33
MuhireIghor <muhireighor123@gmail.com>
2022-11-15 19:01:10 +0200

    question4

---
+++ Exercises/ex4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num1;
+int num2;
+cout<<"Enter the first Number : ";
+cin>>num1;
+cout<<"Enter the second Number : ";
+cin>>num2;
+cout<<"After swapping the first number is" <<num2 <<endl;
+cout<<"After swapping the second number is "<<num1 <<endl;
+    return 0;
+}
