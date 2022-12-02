d9f4a009f9e795977aa1e60ea7a45aad33ee746e
MuhireIghor <muhireighor123@gmail.com>
2022-12-02 19:18:57 +0200

    [updates]:quest one and two using recursive function

--- statements/continue.cpp
+++ statements/continue.cpp
@@ -2,13 +2,20 @@
 using namespace std;
 
 int main(){
-int missed;
-for (int count = 1; count <= 10; ++count ) {
-if ( count == 5 ) {
-missed = count;
-continue; 
-} 
-cout << count << ", ";
-} //end for
-cout <<"The loop skips: "<<missed<<endl;
+// int missed;
+// for (int count = 1; count <= 10; ++count ) {
+// if ( count == 5 ) {
+// missed = count;
+// continue; 
+// } 
+// cout << count << ", ";
+// } //end for
+// cout <<"The loop skips: "<<missed<<endl;
+int x=10, y=20, z=5, t=2, s=7;
+int k=x/y%s*t+y/x*t;
+cout<<k;
+
+int f=x*y/z%s*t-s*x/t;
+cout<<f;
+
 return 0;}
