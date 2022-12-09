66b81607b45b3382984bcf9fe7a4c0d4d30bddcc
MuhireIghor <muhireighor123@gmail.com>
2022-12-09 16:09:24 +0200

    quick sort init

--- Exercises/pmi/factorial.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int factorial(int n){
-    return n*factorial(n-1);
-}
-int main(){
-    cout<<factorial(5);
-    return 0;
-}
--- Exercises/pmi/pointers/incd.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int a=12;
-    cout<<a++;
-    cout<<++a;
-    return 0;
-}
